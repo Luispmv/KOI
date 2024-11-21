f780a16ce4bef607a2d4f7a32a18fb74b144d5ac
Luis <luispmv07@hotmail.com>
2024-11-21 12:05:45 -0600

    Configuraciones iniciales del proyecto

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+// Invocamos a express
+
 const express = require('express');
 const app = express()
 
 app.listen(3000, (req, res)=>{
     console.log("Servidor corriendo en http://localhost:3000")
 })
+
+app.get("/", (req, res)=>{
+    res.send("Hola Mundo")
+})
+
+// Utilizando express url  para capturar los datos del formulario
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+// Invocamos a dotenv
+const dotenv = require("dotenv")
+dotenv.config({path:"./env/.env"})
+
+
+// Configurando el directorio public
+app.use("/resources", express.static("public"))
+app.use("/resources", express.static(__dirname + "/public"))
+
